f8fbdfd5494ee286722aae5b8137638f70b75bf7
rizalw <rizalwidyananda@gmail.com>
2021-08-04 12:39:12 +0700

    refurnish responsive UI

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import *
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 # db = SQLAlchemy(app)
 
 @app.route("/")
