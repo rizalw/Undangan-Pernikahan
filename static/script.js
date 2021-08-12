feee123a5d1134285fbbd4ebc352d6702e0c5d06
rizalw <rizalwidyananda@gmail.com>
2021-08-12 15:19:33 +0700

    countdown timer

--- static/script.js
+++ static/script.js
@@ -250,4 +250,36 @@ function closeCover(){
     cover.style.display = "none"
     music.play()
 }
+
+function countdown(){
+    let countDownDate = new Date("sep 23, 2021 08:00:00").getTime();
+
+    // Run myfunc every second
+    let myfunc = setInterval(function() {
+
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days 
+    document.getElementById("hours").innerHTML = hours 
+    document.getElementById("mins").innerHTML = minutes 
+    document.getElementById("secs").innerHTML = seconds 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = "0"
+        document.getElementById("hours").innerHTML = "0" 
+        document.getElementById("mins").innerHTML = "0"
+        document.getElementById("secs").innerHTML = "0"
+    }
+    }, 1000);
+}
 putHistory();
