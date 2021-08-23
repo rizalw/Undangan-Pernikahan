717ba456685be9384fc781ce8791cffd405db396
rizalw <rizalwidyananda@gmail.com>
2021-08-24 01:25:41 +0700

    fitur komentar unpolished

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from re import A
 from flask import *
+from flask.scaffold import F
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
@@ -44,27 +45,73 @@ tamu_undangan = {
     "M Fahmi Suyuti" : 166
 }
 
-#routing
-
-@app.route("/admin214")
-def admin():
-    return render_template("admin.html")
-
-@app.route("/")
-def index():
-    return render_template("index.html", data = "Nama Pengunjung")
-
 def get_keys(value):
     for x, y in tamu_undangan.items():
         if y == value:
             return x
 
+overlay = True
+
+#routing
+
+@app.route("/admin214")
+def admin():
+    daftar_komentar = komentar.query.order_by(komentar.date_created).all()
+    return render_template("admin.html", daftar_komentar = daftar_komentar)
+
+
+@app.route("/")
+def index():
+    try:
+        global overlay
+        daftar_komentar = komentar.query.order_by(komentar.date_created).all()
+        return render_template("index.html", data = "Nama Pengunjung", id = 0, overlay = overlay, daftar_komentar = daftar_komentar)
+    finally:
+        if overlay is False:
+            overlay = True
+
+
 @app.route("/<int:id>")
 def personalized_index(id):
-    return render_template("index.html", data = get_keys(id))
+    try:
+        global overlay
+        daftar_komentar = komentar.query.order_by(komentar.date_created).all()
+        return render_template("index.html", data = get_keys(id), id = id, overlay = overlay, daftar_komentar = daftar_komentar)
+    finally:
+        if overlay is False:
+            overlay = True
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    delete_data = komentar.query.get_or_404(id)
+    try:
+        db.session.delete(delete_data)
+        db.session.commit()
+        return redirect("/admin214")
+    except:
+        return "Terjadi masalah dalam penghapusan data"
+
+@app.route("/submit/<author>/<int:id>", methods = ["POST"])
+def submit(id, author):
+    author = author
+    comment = request.form["comment"]
+    # print(author, comment)
+    global overlay
+    overlay = False
+    input_baru = komentar(author, comment)
+    try:
+        db.session.add(input_baru)
+        db.session.commit()
+    except:
+        return "Upload Gagal"
+    else:
+        if id == 0:
+            return redirect("/")
+        return redirect("/{}".format(id))
 
 @app.route('/download')
 def download():
     return send_file("./static/images/Tiket-Souvenir.png", as_attachment=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
